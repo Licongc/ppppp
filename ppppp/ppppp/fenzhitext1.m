d539fd8113a2f3c6d2f332482b84693bfcdb6530
licc <licc@liccdemacbook-pro.local>
2017-01-02 17:00:19 +0800

    修改了注释

--- ppppp/ppppp/fenzhitext1.m
+++ ppppp/ppppp/fenzhitext1.m
@@ -11,11 +11,8 @@
 @implementation fenzhitext1
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
+{
     // Drawing code
 }
 */
-
 @end
