8d79f3e8a4a4eb67f3222b7adc61c1203fdd6bb5
licc <licc@liccdemacbook-pro.local>
2017-01-02 16:38:57 +0800

    新建项目

---
+++ ppppp/ppppp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ppppp
+//
+//  Created by licc on 17/1/2.
+//  Copyright © 2017年 li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ppppp/ppppp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ppppp
+//
+//  Created by licc on 17/1/2.
+//  Copyright © 2017年 li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
