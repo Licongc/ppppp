079db384758fced8fa08d4d0f251e258a0bc82f3
licc <licc@liccdemacbook-pro.local>
2017-01-02 16:42:42 +0800

    添加了fenzhi类

---
+++ ppppp/ppppp/fenzhitext1.h
@@ -0,0 +1,13 @@
+//
+//  fenzhitext1.h
+//  ppppp
+//
+//  Created by licc on 17/1/2.
+//  Copyright © 2017年 li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fenzhitext1 : UIView
+
+@end
---
+++ ppppp/ppppp/fenzhitext1.m
@@ -0,0 +1,21 @@
+//
+//  fenzhitext1.m
+//  ppppp
+//
+//  Created by licc on 17/1/2.
+//  Copyright © 2017年 li. All rights reserved.
+//
+
+#import "fenzhitext1.h"
+
+@implementation fenzhitext1
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
